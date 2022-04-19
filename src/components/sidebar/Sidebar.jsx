0b803f62beb232ae752b2609453f9d047fc2ae46
yinger <75009049+yingerliang@users.noreply.github.com>
2022-04-19 17:52:20 +1200

    Added Sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,88 @@
 import './sidebar.scss'
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined'
+import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined'
+import StoreMallDirectoryOutlinedIcon from '@mui/icons-material/StoreMallDirectoryOutlined'
+import CreditCardIcon from '@mui/icons-material/CreditCard'
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined'
+import AssessmentOutlinedIcon from '@mui/icons-material/AssessmentOutlined'
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined'
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined'
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined'
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
 
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">Yinger</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardOutlinedIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <GroupOutlinedIcon className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <StoreMallDirectoryOutlinedIcon className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingOutlinedIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <AssessmentOutlinedIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsActiveOutlinedIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsOutlinedIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutOutlinedIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import './home.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
+      <div className="homeContainer">container</div>
     </div>
   )
 }
